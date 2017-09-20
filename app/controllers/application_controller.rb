387b31c6573e5f57e84aab47c18b559f169c4875
Timothy-Dement <tmdement@ncsu.edu>
2017-09-20 00:15:10 -0400

    Create homepage message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "<h1 style=\"font-family: sans-serif\">Hello, world!</h1>".html_safe
+  end
+
 end
